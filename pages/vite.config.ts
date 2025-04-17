63d929d33e191225217380dacde5606320aabdd4
Benny Schudel <bennyschudel@gmail.com>
2025-04-17 15:04:34 +0200

    feat: add page

---
+++ pages/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import BundleSize from 'vite-plugin-bundlesize';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/ryb-color-picker',
+  build: {
+    sourcemap: 'hidden',
+    chunkSizeWarningLimit: 200,
+  },
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes('-'),
+        },
+      },
+    }),
+    BundleSize({
+      stats: 'all',
+      limits: [
+        {
+          name: '**/*',
+          limit: '200 kB',
+        },
+      ],
+    }),
+  ],
+});
